2e84c5b89e6b8a07e1f8a70d5c1e5045f82bacc6
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 23:41:11 +0100

    Update sequencer.sv

--- part3/sequencer.sv
+++ part3/sequencer.sv
@@ -1,24 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: sequencer
-//            :
-// File name  : sequencer.sv
-//            :
-// Description: Code for M4 Lab exercise
-//            : Outline code for sequencer
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : 
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : 
-//
-// Revision   : Version 1.0 
-/////////////////////////////////////////////////////////////////////
-
 module sequencer (input logic start, clock, Q0, n_rst,
  output logic add, shift, ready, reset);
 
